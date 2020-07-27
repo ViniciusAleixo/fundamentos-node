e333fce3aaf908a285e806d198329a0617ae0717
Administrator <administrator@milenas-iMac.local>
2020-07-27 15:16:56 +0930

    Alter CreateTransactionService

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -11,18 +11,13 @@ class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
   constructor(transactionsRepository: TransactionsRepository) {
-    const { total } = this.transactionsRepository.getBalance();
-
-    if (type === 'outcome' && total < value) {
-      throw new Error(' You do not have enogh balance');
-    }
-
     this.transactionsRepository = transactionsRepository;
   }
 
   public execute({ title, value, type }: Request): Transaction {
-    if (!['income', 'outcome'].includes(type)) {
-      throw new Error('Type invalid');
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new Error(' You do not have enogh balance');
     }
     const transaction = this.transactionsRepository.create({
       title,
